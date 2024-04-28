0e6014ca4ee27593a374e77d946ef3ba7ab338fa
David Richey <darichey1@gmail.com>
2024-04-28 01:41:46 -0500

    Always show velocity visual

--- game/VelocityVisual.gd
+++ game/VelocityVisual.gd
@@ -3,12 +3,20 @@ extends Control
 @onready var player = $"../.."
 @onready var camera = $"../../Head/Camera3D"
 
+func draw_vector(from: Vector3, to: Vector3, color: Color) -> void:
+	draw_line(
+		camera.unproject_position(from),
+		camera.unproject_position(to),
+		color,
+		4
+	)
+
 func _draw() -> void:
-	if player.debug:
-		var color = Color(0, 1, 0)
-		var start = camera.unproject_position(player.global_transform.origin)
-		var end = camera.unproject_position(player.global_transform.origin + player.get_xz_velocity())
-		draw_line(start, end, color, 4)
+	draw_vector(
+		player.global_transform.origin,
+		player.global_transform.origin + player.get_xz_velocity(),
+		Color.GREEN
+	)
 
 func _process(delta: float) -> void:
 	queue_redraw()
